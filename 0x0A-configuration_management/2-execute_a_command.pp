7a82c6ab5bd8561956817154ecc7124c76ce17e9
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-22 15:27:20 -0700

    killing a process using puppet

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+# Using puppet to kill a process in shell
+
+exec { 'killing a process using pkill':
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
+
+}
