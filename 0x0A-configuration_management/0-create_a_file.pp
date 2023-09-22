c729ade8d83e20bd0d8b58a96dd8d1d62f1e3b8c
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-22 15:32:44 -0700

    creating a file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # File for creating a file using puppet
 # The file (School) has permissions 0744, belonging to the user and group
-
 file { '/tmp/school':
   ensure  => present,
-  mode    => '0074',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
