918ca6c21a328bc9308a2b38a20a5798d494b1cc
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-23 15:09:37 -0700

    Exporting it to a json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""API that todo lists of employees"""
+import requests
+import sys
+import json
+
+
+if __name__ == '__main__':
+
+    id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/users'
+
+    employee_url_link = base_url + '/' + id
+    task_url_link = employee_url_link + '/todos'
+
+    employee_url = requests.get(employee_url_link)
+    task_url = requests.get(task_url_link)
+
+    employee_name = employee_url.json()['username']
+    all_tasks = task_url.json()
+
+    dict_ = {id: []}
+    for task in all_tasks:
+        dict_[id].append({
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": employee_name
+            })
+        with open('{}.json'.format(id), 'w') as file_:
+            json.dump(dict_, file_)
